255c19e2d6aed553a3cc4572811784996bcbe7df
Victor Gonzalez <victorgm2709@gmail.com>
2023-10-29 02:59:02 +0100

    Boton crear receta "hecho"

--- app/src/main/java/es/unex/giis/asee/gepeto/view/home/recetas/RecetasFragment.kt
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/home/recetas/RecetasFragment.kt
@@ -7,15 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import es.unex.giis.asee.gepeto.R
 import es.unex.giis.asee.gepeto.databinding.FragmentRecetasBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -27,17 +25,24 @@ class RecetasFragment : Fragment() {
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
     private lateinit var button: Button
+    private lateinit var navController: NavController
     private var _binding: FragmentRecetasBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         // Inflate the layout for this fragment
         _binding = FragmentRecetasBinding.inflate(inflater, container, false)
         setUpTabs()
+
+        navController = findNavController()
+        binding.crearRecetaButton.setOnClickListener {
+            navController.navigate(R.id.action_recetasFragment_to_ingredientesFragment)
+        }
+
         return binding.root
     }
 
