752b19691af09bc7f79d54ec1b84bd4c69b64af8
Victor Gonzalez Morales <victorgm2709@gmail.com>
2023-11-23 03:17:22 +0100

    SCRUM-251 Filtrar Receta

--- app/src/main/java/es/unex/giis/asee/gepeto/view/LoginActivity.kt
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/LoginActivity.kt
@@ -2,10 +2,137 @@ package es.unex.giis.asee.gepeto.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import es.unex.giis.asee.gepeto.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.PreferenceManager
+import es.unex.giis.asee.gepeto.data.Session
+import es.unex.giis.asee.gepeto.database.GepetoDatabase
+
+import es.unex.giis.asee.gepeto.databinding.ActivityLoginBinding
+import es.unex.giis.asee.gepeto.model.User
+import es.unex.giis.asee.gepeto.utils.CredentialCheck
+import es.unex.giis.asee.gepeto.view.home.HomeActivity
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var db: GepetoDatabase
+
+    private val responseLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                with(result.data) {
+                    val name = this?.getStringExtra(JoinActivity.USERNAME).orEmpty()
+                    val password = this?.getStringExtra(JoinActivity.PASS).orEmpty()
+
+                    with(binding) {
+                        etPassword.setText(password)
+                        etUsername.setText(name)
+                    }
+
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "New user ($name/$password) created",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //view binding and set content view
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = GepetoDatabase.getInstance(applicationContext)!!
+
+        //views initialization and listeners
+        setUpUI()
+        setUpListeners()
+
+        readSettings()
     }
+
+    private fun readSettings(){
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this).all
+
+        val rememberMe = preferences["rememberme"] as Boolean? ?: false
+        val username = preferences["username"] as String? ?: ""
+        val password = preferences["password"] as String? ?: ""
+
+        if (rememberMe) {
+            binding.etUsername.setText(username)
+            binding.etPassword.setText(password)
+        }
+    }
+
+    private fun setUpUI() {
+        //get attributes from xml using binding
+    }
+
+    private fun setUpListeners() {
+        with(binding) {
+
+            btLogin.setOnClickListener {
+                correctLogin()
+            }
+
+            btRegister.setOnClickListener {
+                navigateToJoin()
+            }
+
+            btRestore.setOnClickListener {
+                navigateToRestore()
+            }
+
+        }
+    }
+
+    private fun correctLogin(){
+        val credentialCheck = CredentialCheck.login(binding.etUsername.text.toString(), binding.etPassword.text.toString())
+
+        if (credentialCheck.fail) {
+            notifyInvalidCredentials(credentialCheck.msg)
+            return
+        }
+
+        lifecycleScope.launch{
+            val user = db?.userDao()?.findByName(binding.etUsername.text.toString()) //?: User(-1, etUsername.text.toString(), etPassword.text.toString())
+            if (user != null) {
+                // db.userDao().insert(User(-1, etUsername.text.toString(), etPassword.text.toString()))
+                val passwordCheck = CredentialCheck.passwordOk(binding.etPassword.text.toString(), user.password)
+                if (passwordCheck.fail) notifyInvalidCredentials(passwordCheck.msg)
+                else navigateToHomeActivity(user)
+            }
+            else notifyInvalidCredentials("Invalid username")
+        }
+    }
+
+    private fun navigateToHomeActivity(user: User) {
+        Toast.makeText(this, "Welcome ${user.name}!!", Toast.LENGTH_SHORT).show()
+        Session.setValue("user", user)
+        HomeActivity.start(this, user)
+    }
+
+    private fun navigateToJoin() {
+        JoinActivity.start(this, responseLauncher)
+    }
+
+    private fun navigateToRestore() {
+        val showPopUp = PopUpFragment()
+        showPopUp.show(supportFragmentManager, "showPopUp")
+    }
+
+
+    private fun notifyInvalidCredentials(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+
+
 }
--- app/src/main/java/es/unex/giis/asee/gepeto/view/home/HomeActivity.kt
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/home/HomeActivity.kt
@@ -1,11 +1,151 @@
 package es.unex.giis.asee.gepeto.view.home
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import es.unex.giis.asee.gepeto.R
+import es.unex.giis.asee.gepeto.adapters.RecetasAdapter
+import es.unex.giis.asee.gepeto.data.Session
+import es.unex.giis.asee.gepeto.database.GepetoDatabase
 
-class HomeActivity : AppCompatActivity() {
+import es.unex.giis.asee.gepeto.model.User
+import es.unex.giis.asee.gepeto.view.home.recetas.FavoritasFragment
+import es.unex.giis.asee.gepeto.view.home.recetas.HistorialFragment
+import es.unex.giis.asee.gepeto.databinding.ActivityHomeBinding
+import es.unex.giis.asee.gepeto.model.Receta
+import es.unex.giis.asee.gepeto.view.home.recetas.RecetasFragment
+
+import es.unex.giis.asee.gepeto.view.home.recetas.RecetasFragmentDirections
+import kotlinx.coroutines.launch
+import java.util.TreeSet
+
+class HomeActivity :
+    AppCompatActivity(),
+    HistorialFragment.OnRecetaClickListener,
+    FavoritasFragment.OnReceta2ClickListener,
+    IngredientesFragment.OnCrearRecetaListener,
+    ObservacionesFragment.OnGenerarRecetaListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var binding: ActivityHomeBinding
+
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
+
+    companion object {
+        const val USER_INFO = "USER_INFO"
+        fun start( context: Context, user: User ) {
+            val intent = Intent(context, HomeActivity::class.java).apply {
+                putExtra(USER_INFO, user)
+            }
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpUI()
+    }
+
+    private fun setUpUI() {
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recetasFragment,
+                R.id.listaFragment,
+                R.id.ingredientesFragment,
+                R.id.equipamientoFragment
+            )
+        )
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Hide toolbar and bottom navigation when in detail fragment
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if ((destination.id == R.id.recetaDetailFragment) or
+                (destination.id == R.id.observacionesFragment) or
+                (destination.id == R.id.settingsFragment)){
+//                binding.toolbar.menu.clear()
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+
+        // Configure the search info and add any event listeners.
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chooses the "Settings" item. Show the app settings UI.
+            val action = RecetasFragmentDirections.actionHomeToSettingsFragment()
+            navController.navigate(action)
+            true
+        }
+
+        else -> {
+            // The user's action isn't recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onRecetaClick(receta: Receta) {
+        val action = RecetasFragmentDirections.actionRecetasFragmentToRecetaDetailFragment(receta)
+        navController.navigate(action)
+    }
+
+    override fun onReceta2Click(receta: Receta) {
+        val action = RecetasFragmentDirections.actionRecetasFragmentToRecetaDetailFragment(receta)
+        navController.navigate(action)
+    }
+
+    override fun onCrearRecetaClick(ingredientes: TreeSet<String>) {
+        val action = IngredientesFragmentDirections
+            .actionIngredientesFragmentToObservacionesFragment(
+                ingredientes.joinToString(separator = ", ", prefix = "", postfix = ".")
+            )
+        navController.navigate(action)
+    }
+
+    override fun onGenerarRecetaClick(receta: Receta) {
+        val action = ObservacionesFragmentDirections
+            .actionObservacionesFragmentToRecetaDetailFragment(receta)
+        navController.navigate(action)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Session.clear() // Clear session on logout
     }
 }
+
+
