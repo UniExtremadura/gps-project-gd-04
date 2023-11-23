1c8795f41688926452797086cbd7a5a6e8a6ec82
Badr <babbaoui01@gmail.com>
2023-11-23 03:12:39 +0100

    SCRUM-244 Grocery list edit

--- app/src/main/java/es/unex/giis/asee/gepeto/view/LoginActivity.kt
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/LoginActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import es.unex.giis.asee.gepeto.R
 
+
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
