d127e76fde5288876858a648f1424f5d8d9610c0
Manuel Caballero <mcaballeyn@alumnos.unex.es>
2023-11-23 02:48:49 +0100

    SCRUM-239 Realizar recomendación

---
+++ app/src/main/java/es/unex/giis/asee/gepeto/utils/UsefulFunctions.kt
@@ -0,0 +1,54 @@
+package es.unex.giis.asee.gepeto.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.ViewTreeObserver
+import android.widget.EditText
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import es.unex.giis.asee.gepeto.R
+import es.unex.giis.asee.gepeto.adapters.ItemSwapAdapter
+import java.util.TreeSet
+
+fun ocultarBottomNavigation ( view: View, bottomNavigationView: BottomNavigationView ) {
+    view.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+        override fun onPreDraw(): Boolean {
+            // Calcula la diferencia entre la altura original y la altura actual
+            val heightDiff = view.rootView.height - view.height
+
+            // Si la diferencia es significativa, es probable que el teclado esté en pantalla
+            val isKeyboardOpen = heightDiff > view.rootView.height / 4
+
+            // Oculta o muestra la barra de navegación inferior según sea necesario
+            if (isKeyboardOpen) {
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+
+            // Devuelve true para continuar con la animación
+            return true
+        }
+    })
+}
+
+fun filtrarLista ( buscador: EditText, itemSet: TreeSet<String>, adapter: ItemSwapAdapter ) {
+    buscador.addTextChangedListener( object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val text = s.toString().trim()
+            val listaFiltrada = itemSet.filter {
+                it.contains(text, ignoreCase = true)
+            }
+            adapter.swap(TreeSet<String>(listaFiltrada))
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+    } )
+}
---
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/home/ListaFragment.kt
@@ -0,0 +1,109 @@
+package es.unex.giis.asee.gepeto.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import es.unex.giis.asee.gepeto.R
+import es.unex.giis.asee.gepeto.adapters.ItemSwapAdapter
+import es.unex.giis.asee.gepeto.adapters.TodoAdapter
+import es.unex.giis.asee.gepeto.data.Session
+import es.unex.giis.asee.gepeto.data.todosLosIngredientes
+import es.unex.giis.asee.gepeto.databinding.FragmentListaBinding
+import es.unex.giis.asee.gepeto.utils.filtrarLista
+import es.unex.giis.asee.gepeto.utils.ocultarBottomNavigation
+import java.util.TreeSet
+
+class ListaFragment : Fragment() {
+
+    private lateinit var _binding: FragmentListaBinding
+    private val binding get() = _binding
+
+    private lateinit var todoAdapter: TodoAdapter
+    private lateinit var ingredientesAdapter: ItemSwapAdapter
+
+    private fun getSessionIngredients() : TreeSet<String> {
+        val todoList = Session.getValue("todoList") as HashMap<String, Boolean>? ?: hashMapOf()
+        val ingedientesSet = TreeSet(todosLosIngredientes)
+
+        if (todoList.isEmpty()) {
+            return ingedientesSet
+        }
+
+        for ((key, value) in todoList) {
+            ingedientesSet.remove(key)
+        }
+
+        return ingedientesSet
+    }
+
+    private val todoList : HashMap<String, Boolean> =
+        Session.getValue("todoList") as HashMap<String, Boolean>? ?: hashMapOf()
+
+    private var ingredientesSet: TreeSet<String> = TreeSet()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListaBinding.inflate(inflater, container, false)
+        return _binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        setUpAllRecyclerView()
+        setUpSelectedRecyclerView()
+
+        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        ocultarBottomNavigation(view, bottomNavigationView)
+
+        filtrarLista(
+            binding.buscadorDeIngredientes,
+            ingredientesSet,
+            ingredientesAdapter
+        )
+    }
+
+    private fun setUpSelectedRecyclerView() {
+        todoAdapter = TodoAdapter(
+            todoMap = todoList,
+            onClick = {
+                todoAdapter.check(it)
+            },
+            onLongClick = {
+                todoAdapter.remove(it)
+                ingredientesSet.add(it)
+                ingredientesAdapter.add(it)
+            }
+        )
+
+        with(binding.rvIngredientesSeleccionados) {
+            adapter = todoAdapter
+            layoutManager = LinearLayoutManager(context)
+//            setHasFixedSize(true)
+        }
+    }
+
+    private fun setUpAllRecyclerView() {
+        ingredientesAdapter = ItemSwapAdapter(
+            itemSet = ingredientesSet,
+            onClick = {
+                todoAdapter.add(it)
+                ingredientesAdapter.remove(it)
+                ingredientesSet.remove(it)
+            }
+        )
+
+        with(binding.rvTodosIngredientes) {
+            adapter = ingredientesAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+}
