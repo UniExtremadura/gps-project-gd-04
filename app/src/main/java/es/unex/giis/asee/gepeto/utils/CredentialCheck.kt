8c8ba5442509b5e533a461ae088eb5502d13b1a8
Manuel Caballero <mcaballeyn@alumnos.unex.es>
2023-11-23 01:32:49 +0100

    SCRUM-258 Recuperación de contraseña

---
+++ app/src/main/java/es/unex/giis/asee/gepeto/utils/CredentialCheck.kt
@@ -0,0 +1,66 @@
+package es.unex.giis.asee.gepeto.utils
+
+class CredentialCheck private constructor() {
+
+    var fail: Boolean = false
+    var msg: String = ""
+    var error: CredentialError = CredentialError.PasswordError
+
+    companion object{
+
+        private val TAG = CredentialCheck::class.java.canonicalName
+        private const val MINCHARS = 4
+
+        private val checks = arrayOf(
+            CredentialCheck().apply {
+                fail = false
+                msg = "Your credentials are OK"
+                error = CredentialError.Success
+            },
+            CredentialCheck().apply {
+                fail = true
+                msg = "Invalid username"
+                error = CredentialError.UsernameError
+            },
+            CredentialCheck().apply {
+                fail = true
+                msg = "Invalid password"
+                error = CredentialError.PasswordError
+            },
+            CredentialCheck().apply {
+                fail = true
+                msg = "Passwords do not match"
+                error = CredentialError.PasswordError
+            }
+
+        )
+
+        fun login(username: String, password: String): CredentialCheck {
+            return if (username.isBlank() || username.length < MINCHARS) checks[1]
+            else if (password.isBlank() || password.length < MINCHARS) checks[2]
+            else checks[0]
+        }
+
+        fun passwordOk(pass1: String, pass2: String): CredentialCheck {
+            return if (pass1.isBlank() || pass1.length < MINCHARS) checks[2]
+            else if (pass1 != pass2) checks[3]
+            else checks[0]
+        }
+
+        fun newPasswordOk(pass1: String): CredentialCheck {
+            return if (pass1.isBlank() || pass1.length < MINCHARS) checks[2]
+            else checks[0]
+        }
+
+        fun join(username: String, password: String, repassword: String): CredentialCheck {
+            return if (username.isBlank() || username.length < MINCHARS) checks[1]
+            else if (password.isBlank() || password.length < MINCHARS) checks[2]
+            else if (password!=repassword) checks[3]
+            else checks[0]
+        }
+    }
+
+    enum class CredentialError {
+        PasswordError, UsernameError, Success
+    }
+}
--- app/src/main/java/es/unex/giis/asee/gepeto/view/JoinActivity.kt
+++ app/src/main/java/es/unex/giis/asee/gepeto/view/JoinActivity.kt
@@ -2,10 +2,97 @@ package es.unex.giis.asee.gepeto.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import es.unex.giis.asee.gepeto.R
+
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.lifecycle.lifecycleScope
+import es.unex.giis.asee.gepeto.database.GepetoDatabase
+
+import es.unex.giis.asee.gepeto.databinding.ActivityJoinBinding
+import es.unex.giis.asee.gepeto.model.User
+import es.unex.giis.asee.gepeto.utils.CredentialCheck
+import kotlinx.coroutines.launch
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var db: GepetoDatabase
+
+    private lateinit var binding: ActivityJoinBinding
+
+    companion object {
+
+        const val USERNAME = "JOIN_USERNAME"
+        const val PASS = "JOIN_PASS"
+        fun start(
+            context: Context,
+            responseLauncher: ActivityResultLauncher<Intent>
+        ) {
+            val intent = Intent(context, JoinActivity::class.java)
+            responseLauncher.launch(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = GepetoDatabase.getInstance(applicationContext)!!
+
+        //views initialization and listeners
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        with(binding) {
+            btRegister.setOnClickListener {
+                join()
+            }
+        }
+    }
+
+    private fun join() {
+        with(binding) {
+            val check = CredentialCheck.join(
+                etUsername.text.toString(),
+                etPassword.text.toString(),
+                etRepassword.text.toString()
+            )
+            if (check.fail) notifyInvalidCredentials(check.msg)
+            else {
+                lifecycleScope.launch{
+                    val user = User(
+                        null,
+                        etUsername.text.toString(),
+                        etPassword.text.toString()
+                    )
+                    val id =  db?.userDao()?.insert(user)
+
+                    navigateBackWithResult(
+                        User(
+                            id,
+                            etUsername.text.toString(),
+                            etPassword.text.toString()
+                        )
+                    )
+                }
+
+            }
+        }
+    }
+
+    private fun navigateBackWithResult(user: User) {
+        val intent = Intent().apply {
+            putExtra(USERNAME,user.name)
+            putExtra(PASS,user.password)
+        }
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+
+    private fun notifyInvalidCredentials(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
